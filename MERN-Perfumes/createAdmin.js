93803392cb56b55f08aa9edbf38658a3bf97b37d
Med Aura <medaura99@gmail.com>
2026-04-06 11:06:16 +0500

    install mongoURL

--- MERN-Perfumes/createAdmin.js
+++ MERN-Perfumes/createAdmin.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Admin = require("./models/admin");
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL +"/cardsDB");
 
 const createAdmin = async () => {
     await Admin.create({
