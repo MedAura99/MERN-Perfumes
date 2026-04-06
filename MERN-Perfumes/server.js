f74368f2a8fef3c5d85246597ce800f2858e62ca
Med Aura <medaura99@gmail.com>
2026-04-06 12:35:33 +0500

    install mongoURL

--- MERN-Perfumes/server.js
+++ MERN-Perfumes/server.js
@@ -20,6 +20,6 @@ mongoose.connect(process.env.MONGOAUTH_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
